fbfe8d88404d0a458b116772a91b2f915620f75c
ibrahim <hemahero03@gmail.com>
2024-03-20 10:36:20 +0200

    Initial Commit

---
+++ infineScrolling/CollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionViewCell.swift
+//  infineScrolling
+//
+//  Created by ibrahim on 05/03/2024.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var collectionLbl: UILabel!
+    
+    func setUpCell(lblNum : Int) {
+        collectionLbl.text = "\(lblNum)"
+    }
+    
+}
