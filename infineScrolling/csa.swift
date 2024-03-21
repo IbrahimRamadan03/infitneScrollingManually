caac84656dea90057dad9aa59417290b36433898
ibrahim <hemahero03@gmail.com>
2024-03-21 11:41:28 +0200

    ADD VIEW

---
+++ infineScrolling/csa.swift
@@ -0,0 +1,20 @@
+//
+//  csa.swift
+//  infineScrolling
+//
+//  Created by ASTRA Macbook  on 21/03/2024.
+//
+
+import UIKit
+
+class csa: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
