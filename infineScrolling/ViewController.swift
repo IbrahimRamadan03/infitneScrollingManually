e4081824daa2357ed3670638890494d7bfaaeccc
ibrahim <hemahero03@gmail.com>
2024-03-21 11:42:00 +0200

    commit view

--- infineScrolling/ViewController.swift
+++ infineScrolling/ViewController.swift
@@ -7,95 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController ,UICollectionViewDelegate , UICollectionViewDataSource , UIScrollViewDelegate, UICollectionViewDelegateFlowLayout {
+class ViewController: UIViewController  {
     
     
     
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        
-        self.configureCollectionViewLayoutItemSize()
-        
-    }
-    
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-    }
-    
-    
-    let numbers = 50
-    let numOfShownItems = 3
-    var totalNum: Int = 0
-    var currentIndexPath : IndexPath = IndexPath(row: 0, section: 0)
-    var currentOffSet : Double =  0
-    
-    @IBOutlet weak var mainCollection: UICollectionView!
-    
-    let data  = [1,2,3,4,5,6]
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        totalNum = numOfShownItems + data.count
-        return totalNum
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "colCell", for: indexPath) as! CollectionViewCell
-        let currentCell = indexPath.row % data.count
-        print("\(collectionView.contentOffset.x)" + " collection offset")
-        print("\(collectionView.contentSize.width)" + " collection width")
-        print("\(collectionView.contentSize.width / CGFloat(totalNum))" + " collection equation")
-        cell.collectionLbl.text = "\(data[currentCell])"
-        currentIndexPath = indexPath
-        return cell
-    }
-    
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let singleElementSize = mainCollection.contentSize.width / CGFloat(totalNum)
-        if mainCollection.contentOffset.x > singleElementSize*CGFloat(data.count) {
-            mainCollection.contentOffset.x = mainCollection.contentOffset.x - singleElementSize*CGFloat(data.count)
-        }
-        if mainCollection.contentOffset.x < 0 {
-            print("left")
-            mainCollection.contentOffset.x = mainCollection.contentOffset.x + singleElementSize * CGFloat(data.count)
-        }
-        
-    }
-    
-    
-    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
-        
-        targetContentOffset.pointee = scrollView.contentOffset
-        let indexOfMajorCell = self.indexOfMajorCell()
-        let indexPath = IndexPath(row: indexOfMajorCell, section: 0)
-        mainCollection!.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
-    }
-    
-    func calculateSectionInset() -> CGFloat {
-        return 10
-    }
-    
-    private func configureCollectionViewLayoutItemSize() {
-        let inset: CGFloat = calculateSectionInset()
-        let collectionViewLayout = mainCollection.collectionViewLayout as? UICollectionViewFlowLayout
-        collectionViewLayout?.sectionInset = UIEdgeInsets(top: 0, left: inset, bottom: 0, right: inset)
-        collectionViewLayout?.itemSize = CGSize(width: mainCollection!.frame.size.width - inset * 2, height: mainCollection!.frame.size.height)
-    }
-    
-    
-    func indexOfMajorCell() -> Int {
-        let itemWidth = mainCollection.contentSize.width / CGFloat(totalNum)
-        let offsetPerItem = mainCollection.contentOffset.x / itemWidth
-        print("offset per item \(offsetPerItem)")
-        let index = Int(round(offsetPerItem))
-        let numberOfItems = mainCollection.numberOfItems(inSection: 0)
-        print(numberOfItems)
-        let safeIndex = max(0, min(numberOfItems - 1, index))
-        return safeIndex
-    }
     
     
 }
--- infineScrolling/csa.swift
+++ infineScrolling/csa.swift
@@ -1,20 +1,107 @@
 //
-//  csa.swift
+//  customView.swift
 //  infineScrolling
 //
-//  Created by ASTRA Macbook  on 21/03/2024.
+//  Created by ASTRA Macbook  on 20/03/2024.
 //
 
+/*
+- Create Protocol
+    - Function for draw ur cell (height/Width)
+    - Function for setup collection with configue params
+    - Function for control velocity/speed while scrolling
+    - Function for setData
+ */
 import UIKit
 
-class csa: UIView {
+class CustomView: UIView, UICollectionViewDelegate, UICollectionViewDataSource, UIScrollViewDelegate, UICollectionViewDelegateFlowLayout  {
+    
+    //MARK: - Outlets
+    
+    @IBOutlet weak var mainCollection: UICollectionView!
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    //MARK: - Properties
+    
+    let data  = [1,2,3,4,5,6]
+    let numOfShownItems = 3
+    var totalNum: Int = 0
+    var currentIndexPath : IndexPath = IndexPath(row: 0, section: 0)
+    var currentOffSet : Double =  0
+    
+    //MARK: - Override
+    
+    override class func awakeFromNib() {
+        // velocity speed
+    //    self.collection.decelerationRate = UIScrollView.DecelerationRate.fast
     }
-    */
 
+    override func layoutSubviews() {
+        self.configureCollectionViewLayoutItemSize()
+    }
+        
+    //MARK: - CollectionView
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        totalNum = numOfShownItems + data.count
+        return totalNum
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "colCell", for: indexPath) as! CollectionViewCell
+        let currentCell = indexPath.row % data.count
+        print("\(collectionView.contentOffset.x)" + " collection offset")
+        print("\(collectionView.contentSize.width)" + " collection width")
+        print("\(collectionView.contentSize.width / CGFloat(totalNum))" + " collection equation")
+        cell.collectionLbl.text = "\(data[currentCell])"
+        currentIndexPath = indexPath
+        return cell
+    }
+    
+    //MARK: - Configure Cell
+    
+    private func configureCollectionViewLayoutItemSize() {
+        let inset: CGFloat = calculateSectionInset()
+        let collectionViewLayout = mainCollection.collectionViewLayout as? UICollectionViewFlowLayout
+        collectionViewLayout?.sectionInset = UIEdgeInsets(top: 0, left: inset, bottom: 0, right: inset)
+        collectionViewLayout?.itemSize = CGSize(width: mainCollection!.frame.size.width - inset * 2, height: mainCollection!.frame.size.height)
+    }
+
+    //MARK: - ScrollView
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let singleElementSize = mainCollection.contentSize.width / CGFloat(totalNum)
+        if mainCollection.contentOffset.x > singleElementSize*CGFloat(data.count) {
+            mainCollection.contentOffset.x = mainCollection.contentOffset.x - singleElementSize*CGFloat(data.count)
+        }
+        if mainCollection.contentOffset.x < 0 {
+            print("left")
+            mainCollection.contentOffset.x = mainCollection.contentOffset.x + singleElementSize * CGFloat(data.count)
+        }
+    }
+        
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        targetContentOffset.pointee = scrollView.contentOffset
+        let indexOfMajorCell = self.indexOfMajorCell()
+        let indexPath = IndexPath(row: indexOfMajorCell, section: 0)
+        mainCollection!.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+    }
+    
+    //MARK: - Other
+    
+    func calculateSectionInset() -> CGFloat {
+        return 10
+    }
+        
+    func indexOfMajorCell() -> Int {
+        let itemWidth = mainCollection.contentSize.width / CGFloat(totalNum)
+        let offsetPerItem = mainCollection.contentOffset.x / itemWidth
+        let index = Int(round(offsetPerItem))
+        let numberOfItems = mainCollection.numberOfItems(inSection: 0)
+        let safeIndex = max(0, min(numberOfItems - 1, index))
+        return safeIndex
+    }
+    
+  
+    
 }
+
